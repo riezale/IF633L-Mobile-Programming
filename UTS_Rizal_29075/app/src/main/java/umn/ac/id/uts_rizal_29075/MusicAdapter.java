3df959940b83505c74d3a048ad8411da18c8e1a9
Riezale <areszeta9@gmail.com>
2021-03-21 16:04:48 +0700

    UTS_29075

--- UTS_Rizal_29075/app/src/main/java/umn/ac/id/uts_rizal_29075/MusicAdapter.java
+++
@@ -1,9 +0,0 @@
-package umn.ac.id.uts_rizal_29075;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MyVieHolder> {
-    public class MyVieHolder extends RecyclerView.ViewHolder {
-
-    }
-}
