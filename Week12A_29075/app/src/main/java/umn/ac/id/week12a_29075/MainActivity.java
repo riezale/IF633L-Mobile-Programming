6e80bc4959ee4fca0dce96c6e219c2692fb78f5b
Riezale <areszeta9@gmail.com>
2021-05-06 03:06:29 +0700

    Week 12 B

---
+++ Week12A_29075/app/src/main/java/umn/ac/id/week12a_29075/MainActivity.java
@@ -0,0 +1,32 @@
+package umn.ac.id.week12a_29075;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvDaftarSensor;
+    private SensorManager mSensorManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvDaftarSensor = findViewById(R.id.daftarSensor);
+        mSensorManager = (SensorManager)
+                getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> daftarSensor =
+                mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        StringBuilder sensorText = new StringBuilder();
+        for (Sensor currentSensor : daftarSensor ) {
+            sensorText.append(currentSensor.getName()).append(
+                    System.getProperty("line.separator"));
+        }
+        tvDaftarSensor.setText(sensorText);
+    }
+}
